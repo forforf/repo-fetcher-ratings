bebfc440c32a84a61c58c72fc10a8b82854f2be1
David Martin <dmarti21@gmail.com>
2014-02-04 08:40:31 -0500

    adds ApiMismatchError and passing all tests

--- repo-fetcher-ratings.js
+++ repo-fetcher-ratings.js
@@ -4,15 +4,26 @@ angular.module('RepoFetcherRatings', ['GithubRepoFetcher'])
 
   .factory('Repo', function (GithubRepo) {
     var CONFIG = {
-      ratingPrefix: "_rating_:"
+      ratingPrefix: '_rating_:'
     };
+
+    function ApiMismatchError(message) {
+      this.name = 'ApiMismatchError';
+      this.message = (message || '');
+    }
+    ApiMismatchError.prototype = Error.prototye;
 
     var baseModel = {};
 
     function insertRatings(repos){
-      console.debug('insertRatings', repos);
       var reposWithRatings = repos.map(function(repo){
-        var desc = repo.description || '';
+        var desc;
+        if(repo.description){
+          desc = repo.description;
+        } else {
+          repo.__rating = new ApiMismatchError('API missing description field')
+          return repo;
+        }
 
         var r = desc.split(CONFIG.ratingPrefix)[1];
         if(r){
--- test/spec/repo-fetcher-ratings_test.js
+++ test/spec/repo-fetcher-ratings_test.js
@@ -2,7 +2,7 @@
 
 
 describe('RepoFetcherRatings', function(){
-  var repos = [{},{},{}];
+
   beforeEach( function(){
 
 
@@ -23,9 +23,7 @@ describe('RepoFetcherRatings', function(){
       repo = Repo;
       _httpBackend = $httpBackend;
 
-      _httpBackend
-        .when('GET', /api.github.com/)
-        .respond(repos);
+
     }));
 
     it('sanity check', function(){
@@ -35,17 +33,162 @@ describe('RepoFetcherRatings', function(){
     });
 
     describe('.refreshBaseModel', function(){
-      var fetchedRepos;
 
-      it('returns collection', function(){
-        repo.refreshBaseModel('foo', [])
-          .then(function(repos){
-            fetchedRepos = repos;
+      describe('nominal cases', function(){
+        var fetchedRepos;
+
+        var repos = [
+          { description: 'Awesome  _rating_:{"stable":5, "useful":4}' },
+          { description: 'Meh  _rating_:{"stable":5, "useful":4}' },
+          { description: 'Unfinished  _rating_:{"stable":5, "useful":4}' }
+        ];
+
+
+        beforeEach(function(){
+          _httpBackend
+            .when('GET', /api.github.com/)
+            .respond(repos);
+
+          repo.refreshBaseModel('foo', [])
+            .then(function(repos){
+              fetchedRepos = repos;
+            });
+        });
+
+        it('returns collection', function(){
+          fetchedRepos = null;
+          _httpBackend.flush();
+          expect(fetchedRepos.length).toEqual(3);
+        });
+
+        it('each item has rating object', function(){
+          fetchedRepos = null;
+          _httpBackend.flush();
+          fetchedRepos.forEach(function(repo){
+            expect(repo.__rating.stable).toEqual(5);
+            expect(repo.__rating.useful).toEqual(4) ;
           });
-        expect(fetchedRepos).toBeUndefined();
-        _httpBackend.flush();
-        expect(fetchedRepos).toEqual('foo');
+        });
       });
+
+      describe('abnormal cases', function(){
+        describe('malformed JSON', function(){
+          var fetchedRepos;
+
+          var repos = [
+            { description: 'Awesome  _rating_:{"stable":5 "useful":4}' },
+            { description: 'Meh  _rating_:{""stable":5, "useful":4}' },
+            { description: 'Unfinished  _rating_:{"stable":{{}, "useful":4}' }
+          ];
+
+          beforeEach(function(){
+            _httpBackend
+              .when('GET', /api.github.com/)
+              .respond(repos);
+
+            repo.refreshBaseModel('foo', [])
+              .then(function(repos){
+                fetchedRepos = repos;
+              });
+          });
+
+          it('returns error object (for now at least)', function(){
+            fetchedRepos = null;
+            _httpBackend.flush();
+            fetchedRepos.forEach(function(repo){
+              expect(repo.__rating instanceof SyntaxError).toBeTruthy();
+            });
+          });
+        });
+
+        describe('various valid JSON', function(){
+          var fetchedRepos;
+
+          var repos = [
+            { description: 'Awesome  _rating_:["stable":5, "useful":4]' },
+            { description: 'Meh  _rating_:"plain string"' },
+            { description: 'Unfinished  _rating_:{"a":{"stable":3}, "b":{"useful":4}}' }
+          ];
+
+          beforeEach(function(){
+            _httpBackend
+              .when('GET', /api.github.com/)
+              .respond(repos);
+
+            repo.refreshBaseModel('foo', [])
+              .then(function(repos){
+                fetchedRepos = repos;
+              });
+          });
+
+          it('parses, but up to user land to handle', function(){
+            fetchedRepos = null;
+            _httpBackend.flush();
+            fetchedRepos.forEach(function(repo){
+              expect(repo.__rating).toBeDefined();
+            });
+          });
+        });
+
+        describe('missing _rating_: in description', function(){
+          var fetchedRepos;
+
+          var repos = [
+            { description: 'Awesome' },
+            { description: 'Meh  {"a":"B"}' },
+            { description: 'Unfinished:{"a":"B"}' }
+          ];
+
+          beforeEach(function(){
+            _httpBackend
+              .when('GET', /api.github.com/)
+              .respond(repos);
+
+            repo.refreshBaseModel('foo', [])
+              .then(function(repos){
+                fetchedRepos = repos;
+              });
+          });
+
+          it('returns rating of null', function(){
+            fetchedRepos = null;
+            _httpBackend.flush();
+            fetchedRepos.forEach(function(repo){
+              expect(repo.__rating).toBe(null);
+            });
+          });
+        });
+
+        describe('missing description (ie Github API change', function(){
+          var fetchedRepos;
+
+          var repos = [
+            { desc: 'Awesome' },
+            { desc: 'Meh  {"a":"B"}' },
+            { desc: 'Unfinished:{"a":"B"}' }
+          ];
+
+          beforeEach(function(){
+            _httpBackend
+              .when('GET', /api.github.com/)
+              .respond(repos);
+
+            repo.refreshBaseModel('foo', [])
+              .then(function(repos){
+                fetchedRepos = repos;
+              });
+          });
+
+          it('returns rating with ApiMismatchError', function(){
+            fetchedRepos = null;
+            _httpBackend.flush();
+            fetchedRepos.forEach(function(repo){
+              expect(repo.__rating.name).toEqual('ApiMismatchError');
+            });
+          });
+        });
+      });
+
 
 //      beforeEach(inject(function($q, $timeout){
 //        _timeout = $timeout;
